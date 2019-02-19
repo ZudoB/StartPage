b59ccf3f0d3ae5d9c68e222ccee2069a2d783db7
Nick Booth <nick.booth52@gmail.com>
2019-02-19 23:17:10 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const app = new Vue({
+    "el": "#app",
+    "data": {
+        "search": "",
+        "date": "",
+        "hours": "00",
+        "minutes": "00",
+    },
+    "methods": {
+        "keypress": function (ev) {
+            if (ev.key === "Enter") {
+                this.handleResult(0);
+            } else {
+                if (parseInt(ev.key) && ev.altKey) { // is a number
+                    this.handleResult(parseInt(ev.key) - 1);
+                }
+            }
+        },
+        "handleResult": function (id) {
+            if (this.results[id]) {
+                window.location.href = this.results[id].url;
+            }
+        }
+    },
+    "computed": {
+        "results": function () {
+            return providers.filter(provider => provider.display(this.search.trim())).map(provider => {
+                return {
+                    "name": provider.name,
+                    "colour": provider.colour,
+                    "description": provider.description(this.search.trim()),
+                    "url": provider.handler(this.search.trim())
+                }
+            });
+        }
+    },
+    "mounted": function () {
+        this.$nextTick(function () {
+            this.$refs.search.focus();
+        });
+
+        setInterval(() => {
+            const time = moment();
+            this.date = time.format("dddd D MMMM YYYY");
+            this.hours = time.format("HH");
+            this.minutes = time.format("mm");
+        }, 1000);
+    }
+});
+
+document.body.onkeydown = function () {
+    app.$refs.search.focus();
+};
---
+++ providers.js
@@ -0,0 +1,122 @@
+const TWITTER_HANDLE_REGEX = /^@([a-zA-Z0-9_]+)$/;
+const IG_HANDLE_REGEX = /^@([a-zA-Z0-9_.]+)$/;
+const SUBREDDIT_REGEX = /^\/?r\/([a-zA-Z0-9_]+)$/;
+const GH_REPO_REGEX = /^([a-zA-Z0-9_-]{2,})\/([a-zA-Z0-9_-]{2,})$/;
+
+const providers = [
+    {
+        "name": "GitHub",
+        "colour": "#333",
+        "display": function (query) {
+            return !!GH_REPO_REGEX.exec(query);
+        },
+        "description": function (query) {
+            const owner = GH_REPO_REGEX.exec(query)[1];
+            const repo = GH_REPO_REGEX.exec(query)[2];
+            return "View " + owner + "/" + repo;
+        },
+        "handler": function (query) {
+            const owner = GH_REPO_REGEX.exec(query)[1];
+            const repo = GH_REPO_REGEX.exec(query)[2];
+            return "https://github.com/" + owner + "/" + repo;
+        }
+    },
+    {
+        "name": "Reddit",
+        "colour": "#ff4500",
+        "display": function (query) {
+            return !!SUBREDDIT_REGEX.exec(query);
+        },
+        "description": function (query) {
+            const sub = SUBREDDIT_REGEX.exec(query)[1];
+            return "Browse r/" + sub;
+        },
+        "handler": function (query) {
+            const sub = SUBREDDIT_REGEX.exec(query)[1];
+            return "https://reddit.com/r/" + sub;
+        }
+    },
+    {
+        "name": "Twitter",
+        "colour": "#1da1f2",
+        "display": function (query) {
+            return !!TWITTER_HANDLE_REGEX.exec(query);
+        },
+        "description": function (query) {
+            const handle = TWITTER_HANDLE_REGEX.exec(query)[1];
+            return "View @" + handle;
+        },
+        "handler": function (query) {
+            const handle = TWITTER_HANDLE_REGEX.exec(query)[1];
+            return "https://twitter.com/" + handle;
+        }
+    },
+    {
+        "name": "Instagram",
+        "colour": "#c13584",
+        "display": function (query) {
+            return !!IG_HANDLE_REGEX.exec(query);
+        },
+        "description": function (query) {
+            const handle = IG_HANDLE_REGEX.exec(query)[1];
+            return "View @" + handle;
+        },
+        "handler": function (query) {
+            const handle = IG_HANDLE_REGEX.exec(query)[1];
+            return "https://instagram.com/" + handle;
+        }
+    },
+    {
+        "name": "DuckDuckGo",
+        "colour": "#de5833",
+        "display": function () {
+            return true;
+        },
+        "description": function (query) {
+            return "Search for \"" + query + "\"";
+        },
+        "handler": function (query) {
+            return "https://duckduckgo.com/?q=" + encodeURIComponent(query);
+        }
+    },
+    {
+        "name": "Google",
+        "colour": "#4285f4",
+        "display": function () {
+            return true;
+        },
+        "description": function (query) {
+            return "Search for \"" + query + "\"";
+        },
+        "handler": function (query) {
+            return "https://google.com/search?q=" + encodeURIComponent(query);
+        }
+    },
+    {
+        "name": "YouTube",
+        "colour": "#ff0000",
+        "display": function () {
+            return true;
+        },
+        "description": function (query) {
+            return "Search for \"" + query + "\"";
+        },
+        "handler": function (query) {
+            return "https://youtube.com/results?search_query=" + encodeURIComponent(query);
+        }
+    }
+    ,
+    {
+        "name": "NPM",
+        "colour": "#cb3837",
+        "display": function () {
+            return true;
+        },
+        "description": function (query) {
+            return "Search for \"" + query + "\"";
+        },
+        "handler": function (query) {
+            return "https://www.npmjs.com/search?q=" + encodeURIComponent(query);
+        }
+    }
+];
